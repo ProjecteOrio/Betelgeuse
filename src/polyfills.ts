a1b22f3df25387740a35612e545c9cae9b1091eb
ProjecteOrio <projecte.orio@gmail.com>
2019-08-20 00:23:45 +0200

    work-around to use socket.io with Ionic 4 / Angular 6

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -64,3 +64,7 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+
+ // solution found on https://github.com/socketio/socket.io-client/issues/1206
+(window as any).global = window;
